3bf32d0aea062a72965584745a8868104ac2aacc
Tarek592 <muhammadtarek5920@gmail.com>
2025-05-21 04:33:17 +0600

    update method post show data by email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port =process.env.PORT|| 3000
@@ -46,6 +46,27 @@ async function run() {
        const result = await cursor.toArray();
        res.send(result)
     })
+
+  app.get("/plants/:id",async(req,res)=>{
+          const id=req.params.id;
+          const query={_id:new ObjectId(id)}
+           const result = await plantCollection.findOne(query);
+           res.send(result)
+        })
+
+app.get("/plants/id/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await plantCollection.findOne(query);
+  res.send(result);
+});
+
+app.get("/plants/email/:email", async (req, res) => {
+  const email = req.params.email;
+  const query = { userEmail: email };
+  const result = await plantCollection.find(query).toArray();
+  res.send(result);
+});
 
 
     // Send a ping to confirm a successful connection
