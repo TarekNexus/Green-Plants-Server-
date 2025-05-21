5c6ca6153dcf606018c92b7ba78bc8b2c70a062f
Tarek592 <muhammadtarek5920@gmail.com>
2025-05-22 00:10:26 +0600

    green plants server side complete

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
   
-    await client.connect();
+    
     const plantCollection = client.db("PlantsDB").collection("AllPlants");
 
     app.post("/plants", async (req, res) => {
@@ -111,10 +111,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+   
   } finally {
    
   }
